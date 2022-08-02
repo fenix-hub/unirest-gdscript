cfaa5192310a245c64bad6489ee16409b183e934
fenix-hub <n.santilio97@gmail.com>
2022-08-02 23:53:34 +0200

    second commit

--- addons/unirest-gdscript/plugin.gd
+++ addons/unirest-gdscript/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-    add_autoload_singleton("Unirest", "res://addons/unirest-gdscript/src/Unirest.gd")
+    add_autoload_singleton("Unirest", "res://addons/unirest-gdscript/src/unirest.gd")
 
 
 func _exit_tree() -> void:
