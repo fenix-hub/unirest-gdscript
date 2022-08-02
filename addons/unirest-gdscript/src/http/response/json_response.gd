d2ff4ed3d5c950fc4c9c8a5a6ec591bdccd796b0
fenix-hub <n.santilio97@gmail.com>
2022-08-02 23:56:57 +0200

    update json response

--- addons/unirest-gdscript/src/http/response/json_response.gd
+++ addons/unirest-gdscript/src/http/response/json_response.gd
@@ -1,16 +1,15 @@
 extends BaseResponse
 class_name JsonResponse
 
-var body: JsonNode
+var _body: JsonNode
 
 func _parse_body(raw_body: PoolByteArray) -> void:
     var parse: JSONParseResult = JSON.parse(raw_body.get_string_from_utf8())
     if parse.error == OK:
-        body = JsonNode.new(parse.result)
+        _body = JsonNode.new(parse.result)
 
 func _init(body: PoolByteArray, headers: PoolStringArray, status: int).(body, headers, status) -> void:
     pass
 
-# Returns either an ARRAY or a DICTIONARY
 func get_body() -> JsonNode:
-    return body
+    return _body
