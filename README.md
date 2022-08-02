cbad3f34034f01484360951ed0ce2effa89ff814
fenix-hub <n.santilio97@gmail.com>
2022-08-02 23:55:05 +0200

    update gitignore

---
+++ README.md
@@ -0,0 +1,2 @@
+# unirest-gdscript
+Unirest in GDScript: Simplified, lightweight HTTP client library. Godot Engine HTTPClient extension based on Kong Unirest.
---
+++ addons/unirest-gdscript/src/utils/json_node.gd
@@ -0,0 +1,23 @@
+extends Reference
+class_name JsonNode
+
+var result
+
+func _init(result) -> void:
+    self.result = result
+
+func is_array() -> bool:
+    return (typeof(result) == TYPE_ARRAY)
+
+func to_string() -> String:
+    return JSON.print(result)
+
+# Untyped return!
+func get_result():
+    return result
+
+func get_array() -> Array:
+    return result as Array
+
+func get_dictionary() -> Dictionary:
+    return result as Dictionary
